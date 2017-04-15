4d7e45a76b861c1d4b3896bed458bd9b31f7710e
schuhpereira <eduardopereira@ienh.com.br>
2017-04-15 10:41:06 -0300

    Update desligar.sh

--- desligar.sh
+++ desligar.sh
@@ -13,11 +13,12 @@ hora=$(zenity --title="Desligamento do sistema" --text "Em que horário deseja d
 	zenity --info --title="Desligamento do sistema" --text="Tempo restante: $hora"
 
 # Converte a hora digitada em segundos
-tempo=$(echo "()")
+segundos=$(echo "$hora" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
 
-
-while [[ true ]]; do
-	#statements
-	echo "Faltam  para o desligamento do sistema."
+while test "$segundos" -gt 0; do
+	# Mostra na tela o tempo em segundos que resta para o desligamento do sistema
+	echo "Faltam $segundos para o desligamento do sistema."
 	sleep 1
+# Diminui em um segundo
+  segundos=$(( segundos - 1 ))
 done
