b70a41ce2e862c6b6cdbc28c0131fafa94a4d9b3
schuhpereira <eduardopereira@ienh.com.br>
2017-04-15 10:23:41 -0300

    Update desligar.sh

--- desligar.sh
+++ desligar.sh
@@ -6,10 +6,18 @@
 #
 # Script criado para que o usuário possa digitar o horário de desligamento do sistema
 
-
 hora=$(zenity --title="Desligamento do sistema" --text "Em que horário deseja desligar?" --entry)
 
 	sudo shutdown -h $hora | ENTRY=`zenity --title "Digite sua senha" --password`
 
-zenity --info --title="Desligamento do sistema" --text="O sistema será desligado às $hora"
-	
+	zenity --info --title="Desligamento do sistema" --text="Tempo restante: $hora"
+
+# Converte a hora digitada em segundos
+tempo=$(echo "()")
+
+
+while [[ true ]]; do
+	#statements
+	echo "Faltam  para o desligamento do sistema."
+	sleep 1
+done
